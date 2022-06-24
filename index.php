e92c1ea68c03929174d332e69b0d5cc13e2befea
brunocriacoes <br.rafael@outlook.com>
2022-06-24 14:45:07 -0300

    fix

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$urlTema = get_template_directory_uri();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digital Point</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="shortcut icon" href="<?php echo $urlTema; ?>/assets/image/ico.png" type="image/x-icon">
+</head>
+
+<body>
+    <div class="flex content-center items-center h-[100vh]">
+        <img class="h-[100px] mx-auto" src="<?php echo $urlTema; ?>/assets/image/logo.png" alt="Digital Point">
+    </div>
+</body>
+
+</html>
